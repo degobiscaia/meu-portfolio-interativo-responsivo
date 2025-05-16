432faa1f15bd7bba4a11e5899b5d5d75ce8d7c71
Diego Biscaia <diegobiscaiadiego@gmail.com>
2025-05-15 23:39:31 -0300

    Atualização
    Atualização do portfólio, com melhorias, e simplificação de funcionalidades por meio do uso de funções anônimas.

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
+const menuMobile=document.querySelector('.mobile-menu')
+const menuShow=document.getElementById('menuShow')
+const desligar = document.getElementById('desligado')
+const ligar = document.getElementById('ligado')
+
+menuShow.addEventListener("click",()=>{
+  if(menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
         document.querySelector('.icon').src = "img/menu-black_35dp.svg"; 
     } else {
         menuMobile.classList.add('open')
         document.querySelector('.icon').src = "img/menu-close-black-36dp.svg";
-    }
-}
+    }  
+})
 
-function clicou1(){
-    var ligado = document.getElementById('ligado');
-    ligado.style.display = "none";
-    var desligado = document.getElementById('desligado');
-    desligado.style.display = "block";
-    document.body.style.backgroundColor = `#413F42`; 
-}
+ligar.addEventListener("click",()=>{
+    ligar.style.display = "none";
+    document.body.style.backgroundColor = `#413F42`;
+    desligar.style.display = "block";
+})
 
-function clicou2(){
-    var desligar = document.getElementById('desligado');
+desligar.addEventListener("click",()=>{
     desligar.style.display = "none";
-    var ligar = document.getElementById('ligado');
-    ligar.style.display = "block";
     document.body.style.backgroundColor = `#fff`;
-}
+    ligar.style.display = "block";
+})
+
+
+
+
 
 
 
