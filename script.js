17b570de60271519ff1c812176f819ce63834c68
Diego Biscaia <diegobiscaiadiego@gmail.com>
2025-07-22 23:27:16 -0300

    Update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const menuMobile=document.querySelector('.mobile-menu')
-const menuShow=document.getElementById('menuShow')
-const desligar = document.getElementById('desligado')
-const ligar = document.getElementById('ligado')
+const menuMobile=document.querySelector('.mobile-menu');
+const menuShow=document.getElementById('menuShow');
+const desligar = document.getElementById('desligado');
+const ligar = document.getElementById('ligado');
 
 menuShow.addEventListener("click",()=>{
   if(menuMobile.classList.contains('open')) {
@@ -11,19 +11,19 @@ menuShow.addEventListener("click",()=>{
         menuMobile.classList.add('open')
         document.querySelector('.icon').src = "img/menu-close-black-36dp.svg";
     }  
-})
+});
 
 ligar.addEventListener("click",()=>{
     ligar.style.display = "none";
     document.body.style.backgroundColor = `#413F42`;
     desligar.style.display = "block";
-})
+});
 
 desligar.addEventListener("click",()=>{
     desligar.style.display = "none";
     document.body.style.backgroundColor = `#fff`;
     ligar.style.display = "block";
-})
+});
 
 
 
